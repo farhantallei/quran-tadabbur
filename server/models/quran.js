0e50bee47217ad6d1b420944048c2d0aef55526b
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2021-07-29 10:11:30 +0700

    add auto increment id

--- server/models/quran.js
+++ server/models/quran.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
+import autoIncrement from 'mongoose-auto-increment'
 
 const quranSchema = mongoose.Schema({
-    test_id: { type: Number },
     arabic_name: { type: String, required: true },
     latin_name: { type: String, required: true },
     literal: { type: String, required: true },
@@ -10,5 +10,9 @@ const quranSchema = mongoose.Schema({
     mysterious_letters: { type: Boolean, default: false },
     avail: { type: [String], default: [] }
 })
+
+autoIncrement.initialize(mongoose)
+
+quranSchema.plugin(autoIncrement.plugin, { model: 'Quran', field: 'surah_id', startAt: 1 })
 
 export default mongoose.model('Quran', quranSchema)
