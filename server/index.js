b6c99e92f326c4d788132dc0853b8639c193a430
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2021-08-10 07:33:56 +0700

    server deploy commit

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
-import dotenv from 'dotenv'
 
 import quranRoutes from './routes/quran.js'
 
 const app = express()
-dotenv.config()
 
 app.use(express.json({ limit: '30mb' }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
@@ -18,11 +16,12 @@ app.get('/', (req, res) => {
     res.send('Quran Tadabbur data API')
 })
 
-const PORT = process.env.PORT
+const CONNECTION_URL = 'mongodb+srv://farhantallei:419555444Sifrhn@mqcluster0.uhrek.mongodb.net/MQDatabase?retryWrites=true&w=majority'
+const PORT = 5000
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message))
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`))
 
 mongoose.set('useNewUrlParser', true)
 mongoose.set('useFindAndModify', false)
