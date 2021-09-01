0340c92e3224c05d056295a6a578b1ce2e7730e6
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2021-09-01 16:54:13 +0700

    show total verses for every rukus

--- client/website/src/components/Table/TableVerses/TableVerses.js
+++ client/website/src/components/Table/TableVerses/TableVerses.js
@@ -24,12 +24,16 @@ const TableVerses = ({ isLoading, chapter, setVerseInput, ruku, setRuku, setRuku
         <div className="table">
             <div className="table-head">
                 {!isLoading && chapter && chapter.chapter_index}
+                {/* <input className='search' name='search' type='text' /> */}
             </div>
             <div className="table-layout">
                 <div className="table-scroll">
                     <div className="table-container">
                         {isLoading ? 'Tunggu' : !chapter ? 'Error' : !ruku.length ? 'No data' : ruku.map((verses, rukuIndex) => (
                             <div key={rukuIndex} className="ruku-list">
+                                <div style={{ textAlign: 'center', margin: '0 0 24px' }}>
+                                    <h3>Total: {verses.length} ayat</h3>
+                                </div>
                                 {verses.map((verseData, verseIndex) => (
                                     <Verse key={verseIndex} currentVerseIndex={currentVerseIndex} verseData={verseData} rukuIndex={rukuIndex} verseIndex={verseIndex} setRukuIndex={setRukuIndex} setVerseIndex={setVerseIndex} />
                                 ))}
